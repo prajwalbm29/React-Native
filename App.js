83af8502d520c51a0e799ab2f121f2635ab8cdd5
Prajwal B M <bmprajwal43@gmail.com>
2025-01-11 18:28:54 +0530

    Complete SMS App

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import * as SMS from 'expo-sms';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [number, setNumber] = useState('');
+  const [message, setMessage] = useState('');
+
+  const mmsAvailable = async () => {
+    const isAvailable = await SMS.isAvailableAsync();
+    if (isAvailable) {
+      alert("MMS is available");
+    } else {
+      alert("MMS is not availabel");
+    }
+  }
+
+  const sendMMS = async () => {
+    const { result } = await SMS.sendSMSAsync(
+      number,
+      message,
+      // {
+      //   attachments: {
+      //     uri: 'path/myfile.png',
+      //     mimeType: 'image/png',
+      //     filename: 'myfile.png',
+      //   },
+      // }
+    );
+    if (result === "sent") {
+      alert("Message sent sunncessfully");
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Expo MMS Demo</Text>
+
+      <Button
+        title='Check ssm availabality'
+        onPress={mmsAvailable}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder='Enter Phone Number'
+        value={number}
+        onChangeText={setNumber}
+        keyboardType='phone-pad'
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder='Enter Message'
+        value={message}
+        onChangeText={setMessage}
+        keyboardType='text'
+        multiline
+      />
+
+<Button
+        title='Send Message'
+        onPress={sendMMS}
+      />
+
     </View>
   );
 }
@@ -17,4 +75,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontWeight: "600",
+    fontSize: 24
+  },
+  input: {
+    width: 200,
+    height: 50,
+    margin: 20,
+    padding: 10,
+    borderColor: "black",
+    borderWidth: 1,
+  }
 });
